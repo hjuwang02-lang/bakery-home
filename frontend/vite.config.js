b903d9014b1be40bba60acd123de8e6d12ec4f6d
hjuwang02-lang <hjuwang02@gmail.com>
2026-08-03 16:40:08 +0900

    3차수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react' // 또는 사용하는 프레임워크
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bakery-home/',  // 💡 중요: 깃허브에 만들 '저장소 이름'을 양쪽에 슬래시(/)를 붙여 적어주세요!
+  base: '/',
 })
